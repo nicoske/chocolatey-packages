5d49b3f8f69b0478726865ede8bc71c882001d4e
GitHub Action <action@github.com>
2025-05-13 01:03:20 +0000

    Automatic package update

--- automatic/tomcat/tools/chocolateyUninstall.ps1
+++ automatic/tomcat/tools/chocolateyUninstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-$zipContentFolderName = "apache-tomcat-9.0.104"
+$zipContentFolderName = "apache-tomcat-9.0.105"
 $serviceBat = Split-Path -parent $MyInvocation.MyCommand.Definition |
     Join-Path -ChildPath $zipContentFolderName |
     Join-Path -ChildPath bin |
