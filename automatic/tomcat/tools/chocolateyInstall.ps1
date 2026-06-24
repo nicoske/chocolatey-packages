da951a2dcb8cbc461c8c0f4839b32d73f1bffea8
GitHub Action <action@github.com>
2026-06-24 04:11:58 +0000

    Automatic package update

--- automatic/tomcat/tools/chocolateyInstall.ps1
+++ automatic/tomcat/tools/chocolateyInstall.ps1
@@ -1,9 +1,9 @@
 ﻿$ErrorActionPreference = 'Stop'
 $toolsDir = Split-Path -parent $MyInvocation.MyCommand.Definition
 
-$filename32 = "apache-tomcat-9.0.118-windows-x86.zip"
-$filename64 = "apache-tomcat-9.0.118-windows-x64.zip"
-$zipContentFolderName = "apache-tomcat-9.0.118"
+$filename32 = "apache-tomcat-9.0.119-windows-x86.zip"
+$filename64 = "apache-tomcat-9.0.119-windows-x64.zip"
+$zipContentFolderName = "apache-tomcat-9.0.119"
 
 $packageArgs = @{
     packageName = $env:ChocolateyPackageName
--- automatic/tomcat/tools/chocolateyUninstall.ps1
+++ automatic/tomcat/tools/chocolateyUninstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-$zipContentFolderName = "apache-tomcat-9.0.118"
+$zipContentFolderName = "apache-tomcat-9.0.119"
 $serviceBat = Split-Path -parent $MyInvocation.MyCommand.Definition |
     Join-Path -ChildPath $zipContentFolderName |
     Join-Path -ChildPath bin |
